413f5524252208319f83ffb28ebb99f154506a39
Nicholas <nwhitty@google.com>
2025-10-10 10:01:17 -0600

    force re-auth

--- agent/config/config.py
+++ agent/config/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path=".env")
 
 LOG_IDENTIFIER = "PULSE_AGENT"
-AUTHORIZATION_ID = os.getenv("AUTHORIZATION_ID","pulse_agent_auth_2")
+AUTHORIZATION_ID = os.getenv("AUTHORIZATION_ID","pulse_agent_auth_3")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 LOCAL_AUTH_CREDS = os.getenv("CLIENT_JSON_PAYLOAD", "{}")
 TEMPLATE_BUCKET = os.getenv("TEMPLATE_BUCKET_PATH")
